e8e661f37d8d9bfb4bb599d6b3208d5426c25df6
Nandu Anil <nanduanil@gmail.com>
2017-02-05 15:57:24 -0800

    Karatsuba and Mergesort impementation

---
+++ Coursera_Course1/Karatsuba/Karatsuba/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Karatsuba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = "3141592653589793238462643383279502884197169399375105820974944592";
+            var y = "2718281828459045235360287471352662497757247093699959574966967627";
+            //var x = "1234567";
+            //var y = "5678";
+            var mul = KaratsubaMult(x, y);
+            Console.WriteLine("Multiplication = " + mul);
+            Console.WriteLine("Length = " + mul.ToString().Length);
+            Console.ReadLine();
+        }
+
+        static BigInteger KaratsubaMult(string x, string y)
+        {
+            BigInteger mul;
+            var lenFirst = x.Length;
+            var lenSecond = y.Length;
+            var largerLength = (lenSecond > lenFirst) ? lenSecond : lenFirst;
+            x = x.PadLeft((largerLength), '0');
+            y = y.PadLeft((largerLength), '0');
+
+            if (largerLength > 1)
+            {
+                var partition = (int)Math.Ceiling((double)largerLength / 2);
+                var a = x.Substring(0, partition);
+                var b = x.Substring(partition);
+                var c = y.Substring(0, partition);
+                var d = y.Substring(partition);
+
+                Console.WriteLine("a = " + a);
+                Console.WriteLine("b = " + b);
+                Console.WriteLine("c = " + c);
+                Console.WriteLine("d = " + d);
+
+                var aBig = new BigInteger();
+                var bBig = new BigInteger();
+                var cBig = new BigInteger();
+                var dBig = new BigInteger();
+                BigInteger.TryParse(a, out aBig);
+                BigInteger.TryParse(b, out bBig);
+                BigInteger.TryParse(c, out cBig);
+                BigInteger.TryParse(d, out dBig);
+
+                var a_Plus_b = aBig + bBig;
+                var c_Plus_d = cBig + dBig;
+
+                var acBig = KaratsubaMult(a,c);
+                var bdBig = KaratsubaMult(b,d);
+                var ad_Plus_bcBig = (KaratsubaMult(a_Plus_b.ToString(), c_Plus_d.ToString()) - (acBig + bdBig));
+
+                var ac = acBig.ToString();
+                // var bd = bdBig.ToString();
+                var ad_Plus_bc = ad_Plus_bcBig.ToString();
+
+                BigInteger.TryParse(ac.PadRight((ac.Length + 2 * (largerLength - partition)), '0'), out acBig);
+                BigInteger.TryParse(ad_Plus_bc.PadRight((ad_Plus_bc.Length + largerLength - partition), '0'), out ad_Plus_bcBig);
+
+                mul = acBig + ad_Plus_bcBig + bdBig;
+                    
+                    //( ac.ToString()) + ((BigInteger)Math.Pow(10,(largerLength - (long)partition)) * ad_Plus_bc) + bd;
+
+            }
+            else
+            {
+                mul = Convert.ToInt64(x) * Convert.ToInt64(y);
+            }
+            return mul;
+        }
+    }
+}
---
+++ Coursera_Course1/MergeSort/MergeSort/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sortArray = new int[10]; //100 is just an example
+            Random r = new Random(); //add some seed
+            for (int i = 0; i < sortArray.Length; i++)
+                sortArray[i] = r.Next(200);
+            Console.WriteLine("Array to sort :");
+            PrintArray(sortArray);
+            Console.WriteLine();
+
+            sortArray = MergeSort(sortArray);
+            Console.WriteLine("Sorted Array :");
+            PrintArray(sortArray);
+            Console.ReadLine();
+        }
+
+        static int[] MergeSort(int[] sortArray)
+        {
+                int mid = (int)Math.Floor((decimal)sortArray.Length / 2);
+                int[] loArray = new int[mid];
+                int[] highArray = new int[sortArray.Length - mid];
+                Array.Copy(sortArray, 0, loArray, 0, mid);
+                Array.Copy(sortArray, mid, highArray, 0, sortArray.Length - mid);
+
+                if (mid > 1) { MergeSort(loArray); }
+                if(sortArray.Length - mid > 1) { MergeSort(highArray); }
+                
+                //Merge the arrays
+                int loPoint = 0;
+                int hiPoint = 0;
+
+                for (int currentPoint = 0; currentPoint < sortArray.Length; currentPoint++)
+                {
+                    if (loPoint > loArray.Length - 1)
+                    {
+                        sortArray[currentPoint] = highArray[hiPoint];
+                        hiPoint++;
+                    }
+                    else if (hiPoint > highArray.Length - 1)
+                    {
+                        sortArray[currentPoint] = loArray[loPoint];
+                        loPoint++;
+                    }
+                    else
+                    {
+                        if (loArray[loPoint] < highArray[hiPoint])
+                        {
+                            sortArray[currentPoint] = loArray[loPoint];
+                            loPoint++;
+                        }
+                        else
+                        {
+                            sortArray[currentPoint] = highArray[hiPoint];
+                            hiPoint++;
+                        }
+                    }
+                }
+            Console.WriteLine("------");
+            PrintArray(sortArray);
+            return sortArray;
+        }
+
+        static void PrintArray(int[] printArray)
+        {
+            foreach (Object obj in printArray)
+                Console.Write("    {0}", obj);
+            Console.WriteLine();
+        }
+    }
+}
