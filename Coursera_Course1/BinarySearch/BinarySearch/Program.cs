3680078d4e1fa7eec15907bbd908140f9699ec09
Nandu Anil <nandua.dev@gmail.com>
2017-02-09 22:08:09 -0800

    Binary Search Implementation

---
+++ Coursera_Course1/BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] searchArray = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
+            Console.WriteLine(BinarySearchArray(searchArray,32));
+            Console.ReadLine();
+            
+        }
+
+        static int BinarySearchArray(int[] searchArray, int findValue)
+        {
+            var index = -1;
+            var searchScope = searchArray;
+            int count = 1;
+            while (searchScope.Length > 0)
+            {
+                var checkIndex = (int)Math.Floor((double)(searchScope.Length / 2));
+                if (searchScope.Length == 1)
+                {
+                    if (searchScope[checkIndex] == findValue)
+                    {
+                        index = checkIndex;
+                    }
+                    break;
+                }
+                if (searchScope[checkIndex] > findValue)
+                {
+                    searchScope = searchScope.SubArray(0, checkIndex - 1);
+                }
+                else if (searchScope[checkIndex] < findValue)
+                {
+                    searchScope = searchScope.SubArray(checkIndex + 1, searchScope.Length-1);
+                }
+                else
+                {
+                    index = checkIndex;
+                    break;
+                }
+                count++;
+            }
+
+            Console.WriteLine("Iterations : "+ count);
+            return index;
+        }
+    }
+}
---
+++ Coursera_Course1/BinarySearch/BinarySearch/Utilities.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    static class Utilities
+    {
+        public static T[] SubArray<T>(this T[] data, int startIndex, int endIndex)
+        {
+            int length = (endIndex - startIndex)+1;
+            T[] result = new T[length];
+            Array.Copy(data, startIndex, result, 0,  length);
+            return result;
+        }
+    }
+}
