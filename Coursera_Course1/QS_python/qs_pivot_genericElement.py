0a01fd787c0090ee6ddcce9e16d6666e16e8423e
Nandu Anil <nandua.dev@gmail.com>
2017-02-18 14:52:13 -0800

    QuickSort based on any fixed pivot provided as parameter

---
+++ Coursera_Course1/QS_python/qs_pivot_genericElement.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 17 15:22:37 2017
+
+@author: Nandu Anil
+
+This script takes the pivot position as parameter
+"""
+#%%
+def QuickSort(unSortedArray):
+    global countOfComparison
+    global positionOfPivot
+    
+    temp = unSortedArray[positionOfPivot]
+    unSortedArray[positionOfPivot] = unSortedArray[0]
+    unSortedArray[0] = temp
+    
+    pivot = int(unSortedArray[0])
+
+    i = 0
+    j = 1
+    n = len(unSortedArray)
+    #j loops from 1 to n-1 so n-2 loops
+    for item in range(0,n-1):
+        if(int(unSortedArray[j]) >= pivot):
+            j = j + 1
+        elif(int(unSortedArray[j]) < pivot):
+            temp = unSortedArray[j]
+            unSortedArray[j] = unSortedArray[i+1]
+            unSortedArray[i+1] = temp
+            j = j + 1
+            i = i + 1
+    #swap pivot to correct location
+    unSortedArray[0] = unSortedArray[i]
+    unSortedArray[i] = str(pivot)
+    if(i > 0):
+        countOfComparison = countOfComparison + i
+        unSortedArray[0:i] = QuickSort(unSortedArray[0:i])
+    if(i<(n-1)):
+        countOfComparison = countOfComparison + (n - i - 1)
+        unSortedArray[i+1:n] = QuickSort(unSortedArray[i+1:n])
+        
+    return unSortedArray
+
+#%%
+import sys
+
+unsortedFile = open("QuickSort_1.txt")
+intArray = unsortedFile.read().splitlines()
+
+countOfComparison = 0
+positionOfPivot = int(sys.argv[1])
+print(positionOfPivot)
+intArray = QuickSort(intArray)
+print("Count Of Comparisions:",countOfComparison)
+
+outfile = open("Sorted.txt",'w')
+for line in intArray:
+        outfile.write(line + "\n")
+
+unsortedFile.close()
+outfile.close()        
