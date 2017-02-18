795f1541dbc96a28cf4c514bca29d1d6cc59e012
Nandu Anil <nandua.dev@gmail.com>
2017-02-17 16:56:00 -0800

    QuickSort using python : pivot 1st element

---
+++ Coursera_Course1/QS_python/qs_pivot_1stElement.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 17 15:22:37 2017
+
+@author: Nandu Anil
+"""
+
+def QuickSort(unSortedArray):
+    global countOfComparison
+    pivot = unSortedArray[0]
+    
+    if(len(unSortedArray) > 1):
+        i = 0
+        j = 1
+        n = len(unSortedArray)
+        #j loops from 1 to n-1 so n-2 loops
+        for item in range(0,n-1):
+            if(unSortedArray[j] >= pivot):
+                j = j + 1
+            elif(unSortedArray[j] < pivot):
+                temp = unSortedArray[j]
+                unSortedArray[j] = unSortedArray[i+1]
+                unSortedArray[i+1] = temp
+                j = j + 1
+                i = i + 1
+            #swap pivot to correct location
+            unSortedArray[0] = unSortedArray[i]
+            unSortedArray[i] = pivot
+        if(i > 0):
+            countOfComparison = countOfComparison + i
+            QuickSort(unSortedArray[0:i])
+        if(i<(n-1)):
+            countOfComparison = countOfComparison + (n - i - 1)
+            QuickSort(unSortedArray[i+1:n])
+            
+        return unSortedArray
+    else:
+        return unSortedArray
+
+
+
+unsortedFile = open("QuickSort.txt")
+intArray = unsortedFile.read().splitlines()
+
+countOfComparison = 0
+intArray = QuickSort(intArray)
+print("Count Of Comparisions:",countOfComparison)
+
+outfile = open("Sorted.txt",'w')
+
+
+unsortedFile.close()
+outfile.close()        
