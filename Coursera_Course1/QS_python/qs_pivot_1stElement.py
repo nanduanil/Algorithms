ecc3b520a834470a9da619bc4956c6df6ef07412
Nandu Anil <nandua.dev@gmail.com>
2017-02-17 23:52:56 -0800

    Fix qs 1st pivot

--- Coursera_Course1/QS_python/qs_pivot_1stElement.py
+++ Coursera_Course1/QS_python/qs_pivot_1stElement.py
@@ -4,42 +4,38 @@ Created on Fri Feb 17 15:22:37 2017
 
 @author: Nandu Anil
 """
-
+#%%
 def QuickSort(unSortedArray):
     global countOfComparison
-    pivot = unSortedArray[0]
-    
-    if(len(unSortedArray) > 1):
-        i = 0
-        j = 1
-        n = len(unSortedArray)
-        #j loops from 1 to n-1 so n-2 loops
-        for item in range(0,n-1):
-            if(unSortedArray[j] >= pivot):
-                j = j + 1
-            elif(unSortedArray[j] < pivot):
-                temp = unSortedArray[j]
-                unSortedArray[j] = unSortedArray[i+1]
-                unSortedArray[i+1] = temp
-                j = j + 1
-                i = i + 1
-            #swap pivot to correct location
-            unSortedArray[0] = unSortedArray[i]
-            unSortedArray[i] = pivot
-        if(i > 0):
-            countOfComparison = countOfComparison + i
-            QuickSort(unSortedArray[0:i])
-        if(i<(n-1)):
-            countOfComparison = countOfComparison + (n - i - 1)
-            QuickSort(unSortedArray[i+1:n])
-            
-        return unSortedArray
-    else:
-        return unSortedArray
+    pivot = int(unSortedArray[0])
 
+    i = 0
+    j = 1
+    n = len(unSortedArray)
+    #j loops from 1 to n-1 so n-2 loops
+    for item in range(0,n-1):
+        if(int(unSortedArray[j]) >= pivot):
+            j = j + 1
+        elif(int(unSortedArray[j]) < pivot):
+            temp = unSortedArray[j]
+            unSortedArray[j] = unSortedArray[i+1]
+            unSortedArray[i+1] = temp
+            j = j + 1
+            i = i + 1
+    #swap pivot to correct location
+    unSortedArray[0] = unSortedArray[i]
+    unSortedArray[i] = str(pivot)
+    if(i > 0):
+        countOfComparison = countOfComparison + i
+        unSortedArray[0:i] = QuickSort(unSortedArray[0:i])
+    if(i<(n-1)):
+        countOfComparison = countOfComparison + (n - i - 1)
+        unSortedArray[i+1:n] = QuickSort(unSortedArray[i+1:n])
+        
+    return unSortedArray
 
-
-unsortedFile = open("QuickSort.txt")
+#%%
+unsortedFile = open("QuickSort_1.txt")
 intArray = unsortedFile.read().splitlines()
 
 countOfComparison = 0
@@ -47,7 +43,8 @@ intArray = QuickSort(intArray)
 print("Count Of Comparisions:",countOfComparison)
 
 outfile = open("Sorted.txt",'w')
-
+for line in intArray:
+        outfile.write(line + "\n")
 
 unsortedFile.close()
 outfile.close()        
