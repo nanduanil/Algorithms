d852890fc9585e17b6c3acc8ef5ed7c25acc9986
Nandu Anil <nandua.dev@gmail.com>
2017-04-13 14:18:26 -0700

    fix issues.

--- Coursera_Course2/2_SUM_HashTable/2_sum.py
+++ Coursera_Course2/2_SUM_HashTable/2_sum.py
@@ -10,34 +10,38 @@ startTime = time.time()
 inputNumbers = {}
 inputArray = []
 for line in open('2sum.txt'):
+#for line in open('test1.txt'):
     newNum = int(line)
-    if(newNum in inputNumbers):
-        inputNumbers[newNum] = True
-    else:
-        inputNumbers[newNum] = False
-    inputArray.append(newNum)        
+    inputNumbers[newNum] = True
+#    if(newNum in inputNumbers):
+#        inputNumbers[newNum] = True
+#    else:
+#        inputNumbers[newNum] = False
+#    inputArray.append(newNum)        
 length = len(inputNumbers)
 
 print(time.time() - startTime)
 startTime = time.time()
 
 reqValues = {}
-for i in  range(-10000,-9900):
+for i in  range(-10000,10001):
+#for i in  range(30,41):
     reqValues[i] = True
 
 countOfRequired = 0
-for num in inputArray:
+for num in inputNumbers:
     foundValues = []
     for t in reqValues:
         reqNum = t - num     
         if(reqNum in inputNumbers):
-            if(reqNum == num and inputNumbers[num] == False):
+            if(reqNum == num): # and inputNumbers[num] == False):
                 continue
+            #print(t,num,reqNum)
             countOfRequired += 1
             foundValues.append(t)    
     for key in foundValues:
         reqValues.pop(key,None)
-    
+print("Done")    
 print(time.time() - startTime)
 print(countOfRequired)
 
